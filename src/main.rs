d5e926faa718cf2caa2e6d6913a9270d05767789
anastasiajsokol <anastasia.j.sokol@gmail.com>
2023-04-11 22:39:14 -0500

    nicer printing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #![no_std]
 #![no_main]
 
+mod vga;
+
 use core::panic::PanicInfo;
 
 #[panic_handler]
@@ -8,18 +10,16 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-static HELLO: &[u8] = b"Hello World!";
-
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
+    println!("Hello World!");
+    println!("{} + {} = {}", 2, 2, 2 + 2);
 
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
+    for i in 0..100 {
+        println!("Row {}", i);
     }
-    
+
+    print!("Hey!");
+
     loop {}
 }
